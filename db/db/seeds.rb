30ea7f09b9109c77f476923471e0026ebd39bfa5
Daniel Gitau <gitaud959@gmail.com>
2023-03-21 04:16:53 +0300

    Commit message describing the changes made

---
+++ db/db/seeds.rb
@@ -0,0 +1,3 @@
+Plant.create(name: "Rose", image: "rose.jpg", price: 10.99)
+Plant.create(name: "Lily", image: "lily.jpg", price: 12.99)
+Plant.create(name: "Orchid", image: "orchid.jpg", price: 15.99)
